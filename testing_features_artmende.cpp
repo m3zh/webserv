d9784cac4fba724ae3e126560327841d300d9612
artmende <artmende@student.s19.be>
2022-09-09 17:34:07 +0200

    need to adapt code in webserv.cpp, and make select work with read and write

--- testing_features_artmende.cpp
+++ testing_features_artmende.cpp
@@ -6,7 +6,7 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 17:24:29 by artmende          #+#    #+#             */
-/*   Updated: 2022/09/09 11:52:47 by artmende         ###   ########.fr       */
+/*   Updated: 2022/09/09 17:27:50 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 #include <sys/socket.h> // for socket(), bind and listen
 #include <netinet/in.h> // for struct sockaddr_in
 #include <arpa/inet.h> // for htons and inet_addr
+#include <sys/select.h> // for select()
+#include <fcntl.h>
+
+#include <unistd.h> // temp for close
 
 #define PORT 19042
 #define BACKLOG 255
@@ -44,10 +48,40 @@ int	main()
 	listen(server_sock, BACKLOG);
 
 	// define the 2 sets to be used with select and add the server_sock to it
+	fd_set	socket_pool, socket_ready;
+	FD_ZERO(&socket_pool);
+	FD_SET(server_sock, &socket_pool);
 
 	while (true)
 	{
-		
+		std::cout << "Start of the main loop\n";
+		socket_ready = socket_pool;
+		select(FD_SETSIZE, &socket_ready, NULL, NULL, NULL);
+		for (int i = 0; i < FD_SETSIZE; i++)
+		{
+			if (FD_ISSET(i, &socket_ready))
+			{
+				if (i == server_sock)
+				{
+					std::cout << "About to accept\n";
+					// we need to accept this new connection
+					int	new_socket = accept(server_sock, NULL, NULL); // probably NULL is not ok, and we have to store address somewhere
+					//fcntl(i, F_SETFL, O_NONBLOCK);
+					FD_SET(new_socket, &socket_pool);
+				}
+				else
+				{
+					char	buffer[5000];
+					memset(buffer, 0, sizeof(buffer));
+					recv(i, buffer, sizeof(buffer), 0);
+					std::cout << "data received : \n" << buffer << std::endl;
+					send(i, "HTTP/1.1 200 OK\r\n\r\nYOPPP", 24, 0);
+					close(i); ///////
+					FD_CLR(i, &socket_pool);
+					// that's where we receive data, and then respond
+				}
+			}
+		}
 	}
 
 	return 0;
