b1b264ab40ecc39498d138e5e97276efeb133aa1
Milena Lazzaretti <mlazzare@shi-r8-p5.s19.be>
2022-10-12 14:24:08 +0200

    cgi: img upload

--- cgi-bin/upload_file.py
+++ cgi-bin/upload_file.py
@@ -6,33 +6,14 @@ chunk_size = 1024
 
 def cgi_script():
     message = ""
-    # for param in os.environ.keys():
-    #    message += param
-    #    message += ":"
-    #    message += os.environ[param]
-    #    message += "\n"
     directory = os.environ["DIR_UPLOAD"].strip()
 
-    # with sys.stdin as _in:
-    #     body = _in.read(chunk_size)
-    #     while body != b'':
-    #         body += _in.read(chunk_size)
-    #         handler(buffer)
-
-    body = ""
-    for line in sys.stdin:              # read in chunks          
-        body += line
-
-    message += body
+    body = sys.stdin.read()
 
     boundary = body[:body.find("\n")]
     boundary = boundary.strip()
     filename = body[body.find("filename=") + 10:]
     filename = filename[:filename.find("\"")].strip()
-
-    # message += boundary
-    # message += "\n"
-    # message += filename
 
     content = body[body.find("\n"):]
     content = content[:content.find(boundary)].strip()
